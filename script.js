2e48436f1b1655237203fffcda60fab4793a88dd
Gabriel Terrazas Correa <gabrielterrazas21@gmail.com>
2022-06-23 16:29:51 -0400

    userCreateBook function create book in html

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let myLibrary = ["Bible","Musashi","Zen"];
+let myLibrary = [];
+let bookListo = document.getElementById("booksLisuto");
 
 
 // Define the book constructor // 
@@ -10,26 +11,51 @@ function Book(title, author, pages, read ) {
     this.pages = pages
     this.read = read
 
-}
+};
 
 // Add book to the array //
 
 function addBookToLibrary(book) {
 
     myLibrary.push(book);
+}
+
+// Create new book data and push to array //
 
 
+function createNewBookInfo(title,author,pages,read) {
+
+    let book1 = new Book(title, author, pages, read);
+    myLibrary.push(book1);
 }
 
 
 
 
-pageBooks = document.getElementById('booksLisuto')
+
+
+// Create new book user input //
+
+function userCreateBook() {
+
+    let bookNome = document.getElementById("bTitle").value;
+    let bookAutor = document.getElementById("bAuthor").value;
+    let bookPaginas = document.getElementById("bPages").value;
+    let bookRead = document.getElementById("bRead").value;
+
+    createNewBookInfo(bookNome,bookAutor,bookPaginas,bookRead);
+    bookAddPageLoop();
+
+
+    //createNewBookInfo(input input)
+}
+
+
 
 
 // Update the book list and add new books cards //
 
-function bookLoop() {
+function bookAddPageLoop() {
 
     for (let i = 0; i < myLibrary.length; i++) {
 
@@ -41,18 +67,52 @@ function bookLoop() {
         // Create book card heading //
 
         let titleHead = document.createElement("p");
-        let title = document.createTextNode(myLibrary[i]);
+        let title = document.createTextNode(myLibrary[i].title);
+        titleHead.classList.add("bookTitle");
+        titleHead.setAttribute('id', myLibrary[i].title)
         titleHead.appendChild(title);
         bookCard.appendChild(titleHead);
 
         // Create  book card author //
 
         let authorTitle = document.createElement("p");
-        authorTitle.classList("authorTitle");
-        authorTitle.setAttribute('id',`title${}`)
+        let autorT = document.createTextNode(myLibrary[i].author);
+        authorTitle.classList.add("authorTitle");
+        authorTitle.setAttribute('id',myLibrary[i].author);
+        authorTitle.appendChild(autorT);
+        
+
+        // Create book card pages //
+
+        let bookPages = document.createElement("p");
+        let pageT = document.createTextNode(myLibrary[i].pages);
+        bookPages.classList.add("bookPages");
+        bookPages.setAttribute('id',myLibrary[i].pages);
+        bookPages.appendChild(pageT);
+        
+
+        // create "read or note" //
+
+        let readYesNo = document.createElement("p");
+        let yesNo = document.createTextNode(myLibrary[i].read);
+        readYesNo.classList.add("readOrNot");
+        readYesNo.setAttribute('id', myLibrary[i].read);
+        readYesNo.appendChild(yesNo);
+
+        // add elements to the page //
+
+
+        bookCard.appendChild(authorTitle)
+        bookCard.appendChild(bookPages)
+        bookCard.appendChild(readYesNo)
+        bookListo.appendChild(bookCard)
 
     }
 
 
 
 }
+
+
+
+
