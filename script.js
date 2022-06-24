a6998efd7cb69c66dd617a0a1e6340b9817b3eda
Gabriel Terrazas Correa <gabrielterrazas21@gmail.com>
2022-06-24 09:51:14 -0400

    Finish javascript functions, all working

--- script.js
+++ script.js
@@ -20,14 +20,24 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-// Create new book data and push to array //
-
+// Create new book data, check if it's already in the array and push to array //
 
 function createNewBookInfo(title,author,pages,read) {
 
+
     let book1 = new Book(title, author, pages, read);
-    myLibrary.push(book1);
-}
+
+
+
+    if (myLibrary.some(e => e.title === book1.title)) {
+        return 1
+        
+    } else {
+
+        myLibrary.push(book1);
+    }
+    }
+
 
 
 
@@ -43,12 +53,18 @@ function userCreateBook() {
     let bookPaginas = document.getElementById("bPages").value;
     let bookRead = document.getElementById("bRead").value;
 
-    createNewBookInfo(bookNome,bookAutor,bookPaginas,bookRead);
-    bookAddPageLoop();
+    let teste = createNewBookInfo(bookNome,bookAutor,bookPaginas,bookRead);
 
+    if (teste == 1) {
+        console.log("book's already on the list.")
+    }else {
+        bookAddPageLoop();
+    }
 
-    //createNewBookInfo(input input)
-}
+    }
+
+    
+
 
 
 
@@ -57,61 +73,74 @@ function userCreateBook() {
 
 function bookAddPageLoop() {
 
+
     for (let i = 0; i < myLibrary.length; i++) {
 
         // Create book card container //
         let bookCard = document.createElement("div");
         bookCard.classList.add("cardContainer");
         bookCard.setAttribute('id',`bookCard${i}`);
+
+        // Check if element is already on the page //
+
+        let all = document.getElementById(`bookCard${i}`);
+        if (all !== null) {
+
         
-        // Create book card heading //
+        } 
+        else 
+        {
+            // Create book card heading //
 
-        let titleHead = document.createElement("p");
-        let title = document.createTextNode(myLibrary[i].title);
-        titleHead.classList.add("bookTitle");
-        titleHead.setAttribute('id', myLibrary[i].title)
-        titleHead.appendChild(title);
-        bookCard.appendChild(titleHead);
+            let titleHead = document.createElement("p");
+            let title = document.createTextNode(myLibrary[i].title);
+            titleHead.classList.add("bookTitle");
+            titleHead.setAttribute('id', myLibrary[i].title)
+            titleHead.appendChild(title);
+            bookCard.appendChild(titleHead);
 
-        // Create  book card author //
+            // Create  book card author //
 
-        let authorTitle = document.createElement("p");
-        let autorT = document.createTextNode(myLibrary[i].author);
-        authorTitle.classList.add("authorTitle");
-        authorTitle.setAttribute('id',myLibrary[i].author);
-        authorTitle.appendChild(autorT);
-        
+            let authorTitle = document.createElement("p");
+            let autorT = document.createTextNode(myLibrary[i].author);
+            authorTitle.classList.add("authorTitle");
+            authorTitle.setAttribute('id',myLibrary[i].author);
+            authorTitle.appendChild(autorT);
+            
 
-        // Create book card pages //
+            // Create book card pages //
 
-        let bookPages = document.createElement("p");
-        let pageT = document.createTextNode(myLibrary[i].pages);
-        bookPages.classList.add("bookPages");
-        bookPages.setAttribute('id',myLibrary[i].pages);
-        bookPages.appendChild(pageT);
-        
+            let bookPages = document.createElement("p");
+            let pageT = document.createTextNode(myLibrary[i].pages);
+            bookPages.classList.add("bookPages");
+            bookPages.setAttribute('id',myLibrary[i].pages);
+            bookPages.appendChild(pageT);
+            
 
-        // create "read or note" //
+            // create "read or note" //
 
-        let readYesNo = document.createElement("p");
-        let yesNo = document.createTextNode(myLibrary[i].read);
-        readYesNo.classList.add("readOrNot");
-        readYesNo.setAttribute('id', myLibrary[i].read);
-        readYesNo.appendChild(yesNo);
+            let readYesNo = document.createElement("p");
+            let yesNo = document.createTextNode(myLibrary[i].read);
+            readYesNo.classList.add("readOrNot");
+            readYesNo.setAttribute('id', myLibrary[i].read);
+            readYesNo.appendChild(yesNo);
 
-        // add elements to the page //
+            // add elements to the page //
 
 
-        bookCard.appendChild(authorTitle)
-        bookCard.appendChild(bookPages)
-        bookCard.appendChild(readYesNo)
-        bookListo.appendChild(bookCard)
-
+            bookCard.appendChild(authorTitle)
+            bookCard.appendChild(bookPages)
+            bookCard.appendChild(readYesNo)
+            bookListo.appendChild(bookCard)
+        }
     }
 
 
 
 }
+
+
+
 
 
 
